c26536d04660db1806bfdd66fa316c68630c652a
Alex <agordon1123@gmail.com>
2020-04-14 20:30:01 -0400

    completed earliest ancestor challenge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,10 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Todo: create graph structure
-        // Todo: create stack structure
 
         Graph graph = new Graph();
         graph.addVertex(1);
@@ -30,5 +27,45 @@ public class Main {
         graph.addEdge(4, 6);
 
         System.out.println(graph.dfs(1, 6));
+
+        // Earliest ancestor challenge ->
+        // I instantiated a 3-d array and don't know how to fix it :(
+        ArrayList<Integer[][]> ancestorList = new ArrayList<Integer[][]>();
+        ancestorList.add(new Integer[][] {{1, 3}, {2, 3}, {3, 6}, {5, 6}, {5, 7}, {4, 5}, {4, 8}, {8, 9}, {11, 8}, {10, 1}});
+        System.out.println(earliestAncestor(ancestorList, 6));
+    }
+
+    public static int earliestAncestor(ArrayList<Integer[][]> ancestors, int startingVertex) {
+        Graph graph = new Graph();
+        // add to graph
+        for (int i = 0; i < ancestors.get(0).length; i++) {
+            Integer[] current = ancestors.get(0)[i];
+            graph.addVertex(current[0]);
+            graph.addVertex(current[1]);
+            // add edges backward to traverse upward
+            graph.addEdge(current[1], current[0]);
+        }
+
+        HashSet<Integer> visited = new HashSet<Integer>();
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(startingVertex);
+
+        while (queue.size() > 0) {
+            int current = queue.remove();
+            if (!visited.contains(current)) {
+                visited.add(current);
+                // add neighbors
+                HashSet<Integer> neighbors = graph.getNeighbors(current);
+                if (neighbors.size() > 0) {
+                    for (int neighbor: neighbors) {
+                        queue.add(neighbor);
+                    }
+                }
+                if (queue.size() == 0) {
+                    return current;
+                }
+            }
+        }
+        return -1;
     }
 }
